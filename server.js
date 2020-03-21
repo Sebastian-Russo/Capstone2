e83d578b02789ed98246c45d343e5e8489c0f23d
Sebastian Russo <russo.sebastian@gmail.com>
2020-03-21 14:02:01 -0400

    first test status code

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(morgan('common'));
 app.use(express.json());
 
 // CRUD
+app.get('/', (req, res) => {
+    res.status(200)
+})
 
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`server listening on port 8080`)
+});
 
-app.listen(process.env.PORT || 8080);
+module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -5,13 +5,23 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
-// this makes the should syntax available throughout
-// this module
-const should = chai.should();
+const app = require('../server');
 
-const { BlogPost } = require('../models');
-const { closeServer, runServer, app } = require('../server');
-const { TEST_DATABASE_URL } = require('../config');
+const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+describe('server', function() {
+
+    it('should verify that when you hit up the root url for you client, you get a 200 status code and HMTL', function() {
+
+        let res;
+        return chai.request(app)
+            .get('/')
+            .then(_res => {
+                res = _res;
+                expect(res).to.have.status(200);
+            })
+    })
+});
 
