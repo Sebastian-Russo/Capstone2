afc2a19dff5c65f0d3f4710e09420e2903cde530
Sebastian Russo <russo.sebastian@gmail.com>
2020-03-22 11:52:25 -0400

    added files to organize

---
+++ budget/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const {Budget} = require('./models');
+const {router} = require('./router');
+
+module.exports = {Budget, router};
---
+++ budget/router.js
@@ -0,0 +1,13 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {User} = require('./models');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+// CRUD
+
+module.exports = {router};
