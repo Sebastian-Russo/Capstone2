f8dd56104dd99f0899783c3e13db44bb95bceaae
Sebastian Russo <russo.sebastian@gmail.com>
2020-03-22 22:51:16 -0400

    budget object updates

--- budget/models.js
+++ budget/models.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const budgetSchema = mongoose.Schema({
+    monthlyBudget: { type: Number, require: true },
+    costOfLiving: [
+        {
+            item: { type: String, required: true },
+            amount: { type: Number, required: true },
+        }
+    ],
     weeklyBudget: { type: Number, required: true },
     items: [
         {
@@ -18,13 +25,15 @@ budgetSchema.virtual("item").get(function() {
     const itemObj =
         this.items.sort((a, b) => {
             return b.date - a.date;
-        }) [0] || {},
+        }) [0] || {};
         return itemObj.item;
 });
 
 budgetSchema.methods.serialize = function() {
     return {
         id: this._id,
+        monthlyBudget: this.monthlyBudget,
+        costOfLiving: this.costOfLiving,
         weeklyBudget: this.weeklyBudget,
         items: this.items,
     };
--- budget/router.js
+++ budget/router.js
@@ -2,12 +2,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {User} = require('./models');
+const { Budget } = require('./models');
 
 const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
 // CRUD
+
+router.get("/", (req, res) => {
+    res.json(Budget.get())
+});
+
+router.post("/", (req, res) => {
+    const requiredFields = [""]
+})
 
 module.exports = {router};
