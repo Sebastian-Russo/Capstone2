e7208a02b34c94b92ae22881fb25e08fee8cb809
Ossiggy <mrossig201@gmail.com>
2020-04-07 19:30:27 -0400

    added some comments and my own version

--- public/app.js
+++ public/app.js
@@ -7,6 +7,7 @@
 
 const STATE = {
   user: null,
+  // this isnt the schema, and empty array will be fine;
   costOfLiving: [{item: null, amount: null}],
   budget: null,
   weeklyBudget: null,
@@ -14,45 +15,69 @@ const STATE = {
   editing: false
 };
 
+/* 
+  Don't forget, 'costOfLiving' and 'weeklyBudget' are part of your 'budget'.
+  You need to make sure your state accounts for that.
+*/
+
 //update state function 
 const setState = (currentState=STATE, newState) => {
   Object.assign(currentState, newState);
 };
 
 // to update cost of living state 
+/* 
+  MO: I see what you are going for here, and its the right idea, but you shouldn't
+  name it something so generic if it is for such a specific item. Also, try to keep it
+  so you only have one function updating the state. You want to create what you are going
+  to add to the state first, then update the state with setState() (go to line 167-169 of mikeEdits/app.js)
+*/
+
 function addItem(state, item){
   state.costOfLiving.push(item);
 }
-
-
 
 /////// RENDER FUNCTIONS 
 
 function renderWeeklyBudget(state, element){
   const result = `<li> Weekly Budget: ${state.weeklyBudget} </li>`
+  // MO: you should change this to element
   weeklyBudget.html(result)
 }
 
 // render monthly budget function
 function renderMonthlyBudget(state, element){
   const result = `<li> Monthly Budget: ${state.budget} </li>`
+  // MO: you should change this to element
   monthlyBudget.html(result)
 }
 
 // I realized my cost of living has two input fields, how would that work?
+/* 
+  MO:
+  your cost of living handler should look for each input field by name.
+  you should name each input field for what it is (example: cost-of-liv-item);
+  (go to lines 157 thru 175 of mikeEdits/app.js)
+*/
+
+
 function renderCostOfLiving(state, element){
   const result = state.costOfLiving.map(function(item){ //.map(item =>{})
+    /* 
+      MO: these will return 'undefined'. state.costOfLiving is an array of ojbects, not an object.
+      *hint* you may want to change the callback argument so you aren't doing item.item
+    */
     return `<li> ${state.costOfLiving.item} : ${state.costOfLiving.amount} </li>`
   })
   element.html(result)
 }
 
-
-
-
-
-
 // event handlers
+
+/* 
+  MO: these variables that you put before each function should actually go inside the function.
+  Try to limit global vars as much as possible
+*/
 
 const weeklyBudget = $('.weekly-budget');
 function weeklyBudgetHandler(){  
@@ -89,11 +114,6 @@ function costOfLivingHandler(){
 $(weeklyBudgetHandler);
 $(monthlyBudgetHandler);
 $(costOfLivingHandler);
-
-
-
-
-
 
 
 // function renderList(state, element){
