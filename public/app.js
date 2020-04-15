df0c625838715823b719c013e2a016276fd40187
Sebastian Russo <russo.sebastian@gmail.com>
2020-04-14 21:45:21 -0400

    updated cost of living and weekly budget totals

--- public/app.js
+++ public/app.js
@@ -10,6 +10,8 @@ const STATE = {
   // this isnt the schema, and empty array will be fine;
   budget: {
       costOfLiving: [],
+      totalCost: null,
+      totalExpenses: null,
       weeklyBudget: null,
       weeklyItems: [],
       monthlyBudget: null
@@ -54,7 +56,43 @@ function renderCostOfLiving(state, element){
 }
 
 
+function renderTotalAmountCost(state, element){
+  const { totalCost } = state.budget
+  const result = `<div> ${totalCost} </div>`
+  element.html(result)
+}
+
+function renderTotalAmountExpenses(state, element){
+  const { totalExpenses } = state.budget
+  const result = `<div> ${totalExpenses} </div>`
+  element.html(result)
+}
+
 /////////////// EVENT HANDLERS
+
+// total sum of the key values of `costOfLiving` and `weeklyItems`
+function totalCostHandler(){
+  const totalCost = $('.total-cost-of-living');
+  const total = STATE.budget.costOfLiving.reduce(function(acc, x){
+    return acc + Number(x.amount)
+  }, 0)
+
+  const newBudget = Object.assign({}, STATE.budget, {totalCost: total})
+  setState(STATE, {budget: newBudget});
+  renderTotalAmountCost(STATE, totalCost);
+}
+
+function totalExpensesHandler(){
+  const totalExpenses = $('.total-cost-weekly-items');
+  const total = STATE.budget.weeklyItems.reduce(function(acc, x){
+    return acc + Number(x.amount)
+  }, 0)
+
+  const newBudget = Object.assign({}, STATE.budget, {totalExpenses: total})
+  setState(STATE, {budget: newBudget});
+  renderTotalAmountExpenses(STATE, totalExpenses)
+}
+
 
 function weeklyItemsHandler(){
   const weeklyItems = $('.weekly-items');
@@ -63,11 +101,11 @@ function weeklyItemsHandler(){
     const userInputItem = $('input[name="add-input-item"]').val(); // using the name attr of the input element to find the specific one that we want 
     console.log(userInputItem);
     const userInputAmount = $('input[name="add-input-amount"]').val();
-    const newCost = Object.assign({}, STATE.budget, { // Obj.assign( 1st arg = target, 2nd arg = source, 3rd arg = ? )
+    const newCost = Object.assign({}, STATE.budget, { // Obj.assign( 1st arg = target, 2nd arg = source, 3rd arg = a sepparte obj to overwrite current )
       weeklyItems: [...STATE.budget.weeklyItems, {item: userInputItem, amount: userInputAmount}]
     })
     setState(STATE, {budget: newCost});
-    console.log(STATE)
+    totalExpensesHandler();
     renderWeeklyItems(STATE, weeklyItems);
   })
 }
@@ -103,7 +141,7 @@ function costOfLivingHandler(){
       costOfLiving: [...STATE.budget.costOfLiving, {item: userInputItem, amount: userInputAmount}]
     })
     setState(STATE, {budget: newCost});
-    console.log(STATE)
+    totalCostHandler();
     renderCostOfLiving(STATE, costOfLiving);
   })
 }
@@ -115,18 +153,16 @@ $(weeklyBudgetHandler);
 $(monthlyBudgetHandler);
 $(costOfLivingHandler);
 
+
 // BUDGET PAGE
-// have render the right titles and layout
-// print things to screen
-// have amounts right
-// add css
-// fix weekly items add
+// -- add css 
+// -- fix weekly items add
 // thats one page
+// **make cost of living & weekly budget items add key values and produce sum total
+// -- header (the bar at the top)
+
+// **ON THIS STEP NOW--
+// landing page (anybody arrives on before they sign in, welcome to...)
+// sign in and sign up -did html, do i connect to server, or add more to app.js?
 
 // make html dynamic in app.js (do this last)
-
-// header (the bar at the top)
-
-// 2 more PAGES
-// landing page (anybody arrives on before they sign in, welcome to...)
-// sign in and sign up
